92a6bed3c2daae64be0bbb71776bdcffad7db022
tangkai123456 <tangkai123456@live.com>
2016-12-26 17:04:34 +0800

    delete new1.txt

--- 聊天窗口/public/src/common/ajax.js
+++
@@ -1,9 +0,0 @@
-export default function() {
-	var xmlHttpReq;
-	if (window.XMLHttpRequest) {
-		xmlHttpReq = new XMLHttpRequest();
-	} else {
-		xmlHttpReq = new ActiveXObject('Microsoft.XMLHTTP');
-	}
-	return xmlHttpReq
-}
